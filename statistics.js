e3f7372bdee8fd64b3b69027ae9b6e8f0bc45d9a
AhmedAwamleh <awamlehad@icloud.com>
2022-07-20 02:58:51 +0300

    final result

--- statistics.js
+++ statistics.js
@@ -1,61 +1,38 @@
-
-const perentEl = document.getElementById("table2");
-
-const AllFood = [];
-let ID = 1000
-const parentEl = document.getElementById("foood");
-
-function Food(foodID, foodName, foodtype, price,) {
-
-    this.foodID = ID++;
-
-    this.foodName = foodName;
-
-    this.foodtype = foodtype;
-
-    this.price = price;
-
-    AllFood.push(this);
-}
-
-
+const table = document.getElementById("table2");
+const body = document.getElementById("body");
+let AllFood = [];
+let ID = 1000 ; 
 function getData(){
     let retrevdData=localStorage.getItem("food")
-    console.log(retrevdData);
-    console.log(typeof retrevdData);
-  //string to arry
-  
-  let parseData=JSON.parse(retrevdData)
-  console.log(parseData);
-  console.log(typeof parseData);
-  if (parseData!="null"){
-  for (i=0;i<parseData.length;i++){
-    new Food(parseData[i].foodID,parseData[i].foodName,parseData[i].foodtype,parseData[i].price)
-  
-  }}
-  
+    AllFood=JSON.parse(retrevdData);
  }
-
 getData();
-
-Food.prototype.render = function () {
-    document.getElementById("perentEl")
+console.log(AllFood);
+/*
+let arr = [ {name : 'hassan' , age : 20},{name : 'ahmad'},{name : 'laith'},{name : 'alla'} ];
+console.log(typeof arr[0].age);
+*/
+// Objec . 
+// arr []
+function render(food) {
     let trE = document.createElement("tr");
     let tdId = document.createElement("td");
     let tdName = document.createElement("td");
     let tdType = document.createElement("td");
     let tdPrice = document.createElement("td");
-   
-  
-    tdId.textContent = this.foodID;
-    tdName.textContent = this.foodName;
-    tdType.textContent = this.foodtype;
-    tdPrice.textContent = this.price;
-  
+    tdId.textContent = food.foodID;
+    tdName.textContent = food.foodName;
+    tdType.textContent = food.foodtype;
+    tdPrice.textContent = food.price;
+    
     trE.appendChild(tdId);
     trE.appendChild(tdName);
     trE.appendChild(tdType);
-    trE.appendChild(tdPrice);
+    trE.appendChild(tdPrice);  
   
-    perentEl.appendChild(trE);
+    body.appendChild(trE);
   }
+//console.log(2/0);
+for (let index = 0; index < AllFood.length; index++) {
+render(AllFood[index]); 
+}
